3f647d73c4a89a2aac91754be07e53d2675d9f7c
ragul66 <ragul1.gokul2.3@gmail.com>
2024-10-02 21:56:52 +0530

    Home Page finished

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,39 +7,39 @@ const Home = () => {
   return (
     <div
       id="home"
-      className="bg-gradient-to-r from-indigo-900 to-blue-600 min-h-screen p-10"
+      className="bg-gradient-to-r from-indigo-900 to-blue-600 min-h-screen p-10 font-primary"
     >
-      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between  px-4">
+      <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between px-4">
         {/* Left Section */}
-        <div className="text-white text-center md:text-left md:-mt-10">
+        <div className="text-white text-center md:text-left md:-mt-20">
           <h1 className="text-4xl md:text-6xl font-bold mb-4 animate-bottom-to-top">
             Hello I'm
           </h1>
           <h2 className="text-5xl md:text-7xl font-extrabold text-white mb-6">
             PriyaDharshan
           </h2>
-          <p className="flex justify-center items-center text-xl md:text-2xl mb-4  animate-left-to-right text-orange-600 border-2  rounded-full w-fit p-2 font-semibold bg-white cursor-pointer">
-            CivilEngineer <FaArrowRight className="ml-2  mt-1" />
-          </p>
+          <a href="/service">
+            <p className="flex ml-16 md:ml-0 text-xl md:text-2xl mb-4 animate-left-to-right text-orange-600 border-2 rounded-full w-fit p-2 font-semibold bg-white cursor-pointer">
+              CivilEngineer <FaArrowRight className="ml-2 mt-1" />
+            </p>
+          </a>
 
-          <p className="flex flex-center text-lg md:text-xl mb-8 font-semibold animate-scale-up">
+          <p className="text-justify text-sm md:text-base lg:text-lg mb-8 font-medium leading-relaxed px-4 md:px-0 md:max-w-[800px] animate-bottom-to-top">
             Experienced engineer with over 6 years specializing as a Planning
-            Engineer. Proficient in managing projects
-            <br /> such as a 380-flat, 19-floor multistorey residential building
-            at CMK, and overseeing construction of industrial
-            <br /> and commercial structures at URC. At Amaze, excelled in a
-            quality control role focusing on industrial,
-            <br /> commercial, and residential projects. Adept at project
-            planning, coordination, and ensuring adherence to quality standards{" "}
-            <br /> across diverse construction environments. Throughout my
-            career, I have cultivated strong project management
-            <br /> skills, demonstrating my ability to effectively plan,
-            coordinate, and execute projects. I am confident in my
-            <br /> capacity to contribute to and lead project management
-            initiatives successfully.
+            Engineer. Proficient in managing projects such as a 380-flat,
+            19-floor multistorey residential building at CMK, and overseeing
+            construction of industrial and commercial structures at URC. At
+            Amaze, excelled in a quality control role focusing on industrial,
+            commercial, and residential projects. Adept at project planning,
+            coordination, and ensuring adherence to quality standards across
+            diverse construction environments. Throughout my career, I have
+            cultivated strong project management skills, demonstrating my
+            ability to effectively plan, coordinate, and execute projects. I am
+            confident in my capacity to contribute to and lead project
+            management initiatives successfully.
           </p>
 
-          {/* Customizable  buttons*/}
+          {/* Customizable buttons */}
           {/* <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
             <a
               href="#quote"
@@ -57,18 +57,18 @@ const Home = () => {
         </div>
 
         {/* Right Section (Image and Stats) */}
-        <div className="relative">
+        <div className="relative animate-bottom-to-top">
           <img
             src={man}
             alt="James Smith"
-            className="rounded-lg shadow-lg w-72 md:w-96 mx-auto  mb-6"
+            className="rounded-lg shadow-lg w-72 md:w-96 mx-auto mb-6"
           />
-          <div className="absolute top-20 right-20 bg-white p-3 rounded-lg shadow-lg">
+          <div className="absolute top-0 left-40 md:-top-10 md:left-64 bg-white p-3 rounded-lg shadow-lg">
             <p className="text-blue-900 text-xl font-bold">5</p>
             <p className="text-gray-600 text-sm">Years of Success</p>
           </div>
-          <div className="absolute bottom-10 left-10 bg-white p-3 rounded-lg shadow-lg">
-            <p className="text-pink-600 text-xl font-bold">800+</p>
+          <div className="absolute bottom-10 md:bottom-24 md:-left-12 bg-white p-3 rounded-lg shadow-lg">
+            <p className="text-pink-600 text-xl font-bold">12</p>
             <p className="text-gray-600 text-sm">Projects Completed</p>
           </div>
         </div>
