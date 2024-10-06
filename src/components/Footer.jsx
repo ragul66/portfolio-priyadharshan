3027278b126f8b4996bb589c616484ce5877b0d3
ragul66 <ragul1.gokul2.3@gmail.com>
2024-10-06 13:25:32 +0530

    updated name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-indigo-900 to-blue-600 text-white p-4 flex justify-between items-center fixed bottom-0 w-full font-primary">
       <div className="text-md font-semibold">
-        Priyadarshanravikumar@gmail.com {/* Replace with your actual name */}
+        Priyadarshan R {/* Replace with your actual name */}
       </div>
       <a href="#top" className="hover:text-orange-500">
         <FaArrowUp className="h-6 w-6" /> {/* Top arrow icon */}
